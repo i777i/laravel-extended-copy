39c612e11daebae34ca35197f32c669afbea5020
microgreb <lovemicromir@gmail.com>
2018-09-03 13:29:45 +0000

    Initial Commit

---
+++ _ide_helper_models.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\ParentModel
+ *
+ */
+	class ParentModel extends \Eloquent {}
+}
+
+namespace App\Models\Users{
+/**
+ * App\Models\Users\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Client[] $clients
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Token[] $tokens
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ * @property bool $verified
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Users\User whereVerified($value)
+ */
+	class User extends \Eloquent {}
+}
+
