cd35ca37d1bf2ad28197d54de0769b436b24be82
microgreb <lovemicromir@gmail.com>
2018-09-03 14:03:58 +0000

    Helpers autoload. Auth User shortcut for both web and api.

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-09-03 12:03:12.
+    * Generated on 2018-09-03 13:54:33.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.37 on 2018-09-03 12:03:11.
+ * Generated for Laravel 5.6.37 on 2018-09-03 13:54:33.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
